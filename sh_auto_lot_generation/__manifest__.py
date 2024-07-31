401960d52050724b83c87e6f4553a4655dd14256
Oleksii Panpukha <opanpukha@trilab.pl>
2024-07-31 15:47:51 +0200

    sh_auto_lot_generation v17.0.1.0.0, port to R17

---
+++ sh_auto_lot_generation/__manifest__.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+# Part of Softhealer Technologies
+
+{
+    'name': 'Auto Lot Generation',
+    'author': 'Softhealer Technologies',
+    'website': 'https://www.softhealer.com',
+    'support': 'support@softhealer.com',
+    'category': '',
+    'summary': '',
+    'version': '17.0.1.0.0',
+    'depends': ['stock', 'stock_barcode'],
+    'data': [
+        'data/ir_sequence_data.xml',
+        'views/stock.xml',
+    ],
+
+    'images': ['', ],
+    'live_test_url': '',
+    'auto_install': False,
+    'installable': True,
+    'application': True,
+    'price': '',
+    'license': 'OPL-1',
+    'currency': ''
+}
